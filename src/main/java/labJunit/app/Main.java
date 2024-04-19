f700ad1dd0d1f56ab0cf953ca3494ffe6708da72
Olivia Moreau <moreauolivia49@gmail.com>
2024-04-19 15:51:12 +0200

    feat: add instance of compteBancaire in main

--- src/main/java/labJunit/app/Main.java
+++ src/main/java/labJunit/app/Main.java
@@ -5,12 +5,16 @@ public class Main {
 	public static void main(String[] args) {
 		Calculatrice c = new Calculatrice();
 		
-		//System.out.println("Donnez un nombre");
 		double s = c.somme(10, 20);
 		System.out.println("10 + 20 = " +s);
 		
 		double p = c.produit(5, 7);
 		System.out.println("5 * 7 = " + p);
+		
+		CompteBancaire cb = new CompteBancaire(100);
+		
+		double newSolde = cb.debiterSolde(25); 
+		System.out.println("Le nouveau solde de mon compte est : " + newSolde);
 		
 	}
 
